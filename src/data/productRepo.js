77cc29693dd62afd50d2bf4dda7f576d1e22d8d2
SpeedosDK <casperlading2@gmail.com>
2026-01-14 00:38:12 +0100

    Lavet Repo og Service lag

---
+++ src/data/productRepo.js
@@ -0,0 +1,46 @@
+const Product = require("../models/product");
+
+const POPULATE_FIELDS = 'category subcategory brand size condition color material tags seller';
+async function createProduct(productData) {
+    const newProduct = new Product(productData);
+    return await newProduct.save();
+}
+
+// Til forsiden
+async function allProducts(page = 1, limit = 20) {
+    const skip = (page - 1) * limit;
+    return await Product.find({}).skip(skip).limit(limit)
+        .populate(POPULATE_FIELDS).lean();
+}
+
+async function getProductById(productId) {
+    return await Product.findById(productId)
+        .populate(POPULATE_FIELDS).lean();
+}
+// Til specfik side fx. Mænd -> Clothing -> T-Shirt
+async function findProducts(filters, page = 1, limit = 20) {
+    const skip = (page - 1) * limit;
+
+    return await Product.find(filters)
+        .skip(skip).limit(limit)
+        .populate(POPULATE_FIELDS).lean();
+}
+
+async function updateProduct(productId, productData) {
+    const updatedProduct = await Product.findByIdAndUpdate( productId, productData, { new: true, runValidators: true } )
+        .populate(POPULATE_FIELDS);
+    return updatedProduct;
+}
+async function deleteProduct(productId) {
+    const deltedProduct = await Product.findByIdAndDelete(productId);
+    return deltedProduct;
+}
+
+module.exports = {
+    createProduct,
+    allProducts,
+    getProductById,
+    findProducts,
+    updateProduct,
+    deleteProduct
+}
---
+++ src/services/productService.js
@@ -0,0 +1,33 @@
+const productRepo = require("../data/productRepo");
+
+async function createProduct(productData) {
+    return await productRepo.createProduct(productData);
+}
+
+async function readAllProducts(page, limit) {
+    return await productRepo.allProducts(page, limit);
+}
+async function findProducts(filters, page, limit) {
+    return await productRepo.findProducts(filters, page, limit);
+}
+
+async function getProductById(productId) {
+    return await productRepo.getProductById(productId);
+}
+
+async function updateProduct(productId, productData) {
+    return await productRepo.updateProduct(productId, productData);
+}
+
+async function deleteProduct(productId) {
+    return await productRepo.deleteProduct(productId);
+}
+
+module.exports = {
+    createProduct,
+    readAllProducts,
+    findProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct,
+}
