bc1fadcbfa48cf02a3428ea13b7792afb231c762
SpeedosDK <casperlading2@gmail.com>
2026-01-17 00:26:46 +0100

    bug fix

--- src/data/productRepo.js
+++ src/data/productRepo.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 
 const POPULATE_FIELDS = 'category subcategory brand size condition color material tags seller';
 async function createProduct(productData) {
